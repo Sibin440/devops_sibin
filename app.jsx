dd1d67b620f6bd9c6ae27e24097817dec607e3b5
Sibin440 <sibin9122@gmail.com>
2025-10-28 11:58:13 +0530

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import react, { useEffect, useState } from "react";
+import react from{} 
+useSyncExternalStore.inputdate();
+useSyncExternalStore.inputday();
+useSyncExternalStore.inputmonth();
+useSyncExternalStore.inputyear();
+currendate=inputdate()-currentdate();
+const(useState,useEffect)
